c29bc8c1e1cbe7ccc3dd743ef0058aa0b00a5615
youllz <youssoufzerbo01@gmail.com>
2023-09-12 09:00:04 +0000

    add toast notification

---
+++ backend/pb_migrations/1694468968_updated_users.js
@@ -0,0 +1,36 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  collection.viewRule = " @request.auth.id  = ''"
+  collection.options = {
+    "allowEmailAuth": true,
+    "allowOAuth2Auth": true,
+    "allowUsernameAuth": true,
+    "exceptEmailDomains": null,
+    "manageRule": null,
+    "minPasswordLength": 8,
+    "onlyEmailDomains": null,
+    "requireEmail": false
+  }
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  collection.viewRule = null
+  collection.options = {
+    "allowEmailAuth": true,
+    "allowOAuth2Auth": true,
+    "allowUsernameAuth": true,
+    "exceptEmailDomains": null,
+    "manageRule": null,
+    "minPasswordLength": 8,
+    "onlyEmailDomains": null,
+    "requireEmail": true
+  }
+
+  return dao.saveCollection(collection)
+})
